b9e442bb6c02e475debe3d291f7c12c05dfb8aa2
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2025-11-20 23:03:42 +0300

    chore: .env support

--- timetable_project/wsgi.py
+++ timetable_project/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timetable_project.settings')
